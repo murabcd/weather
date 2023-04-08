4c4481b7db78d42e2c50986cbefc10d465dd40aa
muradpm <abdulkadyr0v@yandex.ru>
2023-04-09 01:20:38 +0300

    Update app.js
    updated time to display the correct weather icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,24 +28,33 @@ window.addEventListener("load", () => {
         const weatherApi = `https://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${apiKey}&metric=true`;
         const response = await fetch(weatherApi);
         const data = await response.json();
-        console.log(data);
+        // console.log("API response:", data);
 
         // Update UI elements with fetched data
         temperatureDegree.textContent =
           data.DailyForecasts[0].Temperature.Maximum.Value.toFixed(1); // Display temperature in Celsius
         temperatureDescription.textContent = data.Headline.Text;
 
-        // Display weather icon
-        const iconNumber = data.DailyForecasts[0].Day.WeatherIcon;
+        // Determine whether it's day or night
+        const currentTime = new Date();
+        const currentHour = currentTime.getHours();
+        const isDaytime = currentHour >= 6 && currentHour < 18;
+
+        // Get the appropriate weather icon number
+        const iconNumber = isDaytime
+          ? data.DailyForecasts[0].Day.Icon
+          : data.DailyForecasts[0].Night.Icon;
+
         if (iconNumber) {
           const iconUrl = `https://developer.accuweather.com/sites/default/files/${
             iconNumber < 10 ? "0" : ""
           }${iconNumber}-s.png`;
+          // console.log("Icon URL:", iconUrl); // Log the icon URL
           weatherIcon.setAttribute("src", iconUrl);
         } else {
-          console.log("Weather icon number not found in the API response.");
+          // console.log("Weather icon number not found in the API response.");
         }
-      }; // The closing bracket for getWeatherData should be here
+      };
 
       const locationData = await getLocationData();
       const locationKey = locationData.Key;
